0647506dae065697fabd5b97d192f368af7dfef6
Motilla <josmotgal@gmail.com>
2019-02-19 20:59:19 +0100

    RestServer -usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express');
+
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,28 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario')
+
+mongoose.connect(process.env.URLDB, (err, resp) => {
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
 });
 
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-    res.json({ body })
-});
-
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto:', process.env.PORT);
